2978048fe3d993f26208426f9d5e07e7041aa1ac
DuoCaiXie <Duocaixie@126.com>
2020-06-29 21:55:57 +0800

    快速原型开发建议播放器

---
+++ 单文件组件/test.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+      <h1 @click="func">你好</h1>
+      <h2>{{food}}</h2>
+      <ul>
+          <li v-for="(item,index) in list" :key='index'>{{item}}</li>
+          
+      </ul>
+  </div>
+</template>
+
+<script>
+import './css/style.css'
+export default {
+    data(){
+        return {
+            food:"西兰花炒蛋",
+            list:['1','2','3']
+        }
+    },
+    methods:{
+        func(){
+            alert('你好么')
+        }
+    }
+}
+</script>
+
+<style>
+    #app{
+       background-color: rebeccapurple; 
+    }
+</style>
