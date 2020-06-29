cadb839466bc534bf13864a7ef9945bb9c4194dc
DuoCaiXie <Duocaixie@126.com>
2020-06-29 22:12:26 +0800

    组件化开发 播放器

---
+++ 组件化开发/App.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <player></player>
+  <player></player>
+  <player></player>
+</div>
+  
+</template>
+
+<script>
+// 导入组件
+import player from './components/player.vue'
+export default {
+  components:{
+    player
+  }
+}
+</script>
+
+<style>
+
+</style>
